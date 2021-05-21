18bf7e63015694891be6f924212fc8526b42f6e2
Ardi Germenji <67866525+a-pyro@users.noreply.github.com>
2021-05-21 16:50:13 +0200

    push

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,47 +2,66 @@ import React from 'react';
 import { Row } from 'react-bootstrap';
 import { connect } from 'react-redux';
 
-const Player = () => (
-  <div className='container-fluid fixed-bottom bg-container pt-1'>
-    <Row>
-      <div className='col-lg-10 offset-lg-2'>
-        <Row>
-          <div className='col-6 col-md-4 col-lg-2 offset-3 offset-md-4 offset-lg-5 playerControls mt-1'>
-            <Row>
-              <a href='/'>
-                <img src='/playerbuttons/Shuffle.png' alt='shuffle' />
-              </a>
-              <a href='/'>
-                <img src='/playerbuttons/Previous.png' alt='shuffle' />
-              </a>
-              <a href='/'>
-                <img src='/playerbuttons/Play.png' alt='shuffle' />
-              </a>
-              <a href='/'>
-                <img src='/playerbuttons/Next.png' alt='shuffle' />
-              </a>
-              <a href='/'>
-                <img src='/playerbuttons/Repeat.png' alt='shuffle' />
-              </a>
-            </Row>
-          </div>
-        </Row>
-        <Row className='justify-content-center playBar py-3'>
-          <div className='col-8 col-md-6'>
-            <div id='progress'>
-              <div
-                className='progress-bar'
-                role='progressbar'
-                aria-valuenow={0}
-                aria-valuemin={0}
-                aria-valuemax={100}
-              ></div>
-            </div>
-          </div>
-        </Row>
-      </div>
-    </Row>
-  </div>
-);
+const mapStateToProps = (state) => ({
+  song: state.playingSong.song,
+});
+const songInfoStyle = {
+  position: 'absolute',
+  color: 'white',
+  display: 'flex',
+  flexDirection: 'column',
+  left: '8%',
+  top: '100%',
+};
 
-export default Player;
+const Player = ({ song }) => {
+  console.log(song);
+  return (
+    <div className='container-fluid fixed-bottom bg-container pt-1'>
+      <Row>
+        <div className='col-lg-10 offset-lg-2'>
+          <Row style={{ position: 'relative' }}>
+            <div style={songInfoStyle}>
+              {/* <img src={song.md5_image} alt='singerpic' /> */}
+              <span>{song.title_short}</span>
+            </div>
+            <div className='col-6 col-md-4 col-lg-2 offset-3 offset-md-4 offset-lg-5 playerControls mt-1'>
+              <Row>
+                <a href='/'>
+                  <img src='/playerbuttons/Shuffle.png' alt='shuffle' />
+                </a>
+                <a href='/'>
+                  <img src='/playerbuttons/Previous.png' alt='shuffle' />
+                </a>
+                <a href='/'>
+                  <img src='/playerbuttons/Play.png' alt='shuffle' />
+                </a>
+                <a href='/'>
+                  <img src='/playerbuttons/Next.png' alt='shuffle' />
+                </a>
+                <a href='/'>
+                  <img src='/playerbuttons/Repeat.png' alt='shuffle' />
+                </a>
+              </Row>
+            </div>
+          </Row>
+          <Row className='justify-content-center playBar py-3'>
+            <div className='col-8 col-md-6'>
+              <div id='progress'>
+                <div
+                  className='progress-bar'
+                  role='progressbar'
+                  aria-valuenow={0}
+                  aria-valuemin={0}
+                  aria-valuemax={100}
+                ></div>
+              </div>
+            </div>
+          </Row>
+        </div>
+      </Row>
+    </div>
+  );
+};
+
+export default connect(mapStateToProps)(Player);
